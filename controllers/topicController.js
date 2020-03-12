d227f1f1c89fba72e210f7feb82e81ba69bfd889
mmustTech <stevetech.2020@hotmail.com>
2020-03-12 15:48:05 +0100

    some layouts changed

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -5,26 +5,26 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 exports.getTopicWithIncViews = catchAsync(async (req, res, next) => {
-    let query = Topic.findById(req.params.id);
-    // if (popOptions) query = query.populate(popOptions);
+  let query = Topic.findById(req.params.id);
+  // if (popOptions) query = query.populate(popOptions);
 
-    const doc = await query;
+  const doc = await query;
 
-    if (!doc) {
-      return next(new AppError('no document found with this ID', 404));
+  if (!doc) {
+    return next(new AppError('no document found with this ID', 404));
+  }
+
+  await Topic.findByIdAndUpdate(
+    doc.id,
+    { $inc: { views: 1} } // }, 
+  )
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: doc
     }
-
-    await Topic.findByIdAndUpdate(
-        doc.id,
-        { $inc: { views: 1} } // }, 
-    )
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        data: doc
-      }
-    });
+  });
 });
 
 exports.getAllTopics = factory.getAll(Topic);
